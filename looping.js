0068ebf9f82e7392622719662f696034bf6c3127
shanawazz-dev <shanawazz-dev>
2025-01-08 10:27:10 +0530

    looping

---
+++ looping.js
@@ -0,0 +1,21 @@
+// for(let i=0;i<=100;i++){
+//     console.log(i)
+// }
+
+
+// for(let i=2;i<=100;i+=2){
+//     console.log(i)
+// }
+
+// for(let i=1;i<=100;i+=2){
+//     console.log(i)
+// }
+
+let n=100;
+for(let i=0;i<=n;i++){
+    if(i%2==0){
+        console.log(i + " is a even number");
+    }else{
+        console.log(i +  " is a odd number");
+    }
+}
